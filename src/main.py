69a564c1a275e663090712571f9d7306fd9dcad4
hatatat-dev <hatatat@glowstonemania.com>
2024-09-13 19:43:38 -0700

    [replay] program to replay recorded logs

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env -S PYTHONPATH=../telemetry python3
 
 from lib.log import *
-from high_stakes.events import *
+from lib.log_reader import *
+from high_stakes.catalog import *
+from lib.replay import *
 
-open_log("manual.csv")
+open_log("replay.csv")
 
+log_reader = LogReader("manual.csv")
 
-init_event_handling()
+replay(catalog, log_reader)
+
+close_log()
